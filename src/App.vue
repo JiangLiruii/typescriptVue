5b51ed5ac1fea3676f930b2896608dd7ac58506f
JiangLirui <535904200@qq.com>
2019-09-08 19:18:16 +0800

    add @props & @propSync

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app">
+    <h1>I'm parent</h1>
+    <h2>Parent name data: <input type="text" v-model="name"></h2>
+    <h2>{{`Parent age data: ${age}`}}</h2>
     <img alt="Vue logo" src="./assets/logo.png">
     <FilterComponent/>
     <hr>
     <DirectiveComponent/>
+    <hr>
+    <PropComponent :name="name" :age.sync="age" />
   </div>
 </template>
 
@@ -11,14 +16,20 @@
 import { Component, Vue } from 'vue-property-decorator';
 import FilterComponent from './components/filter.vue';
 import DirectiveComponent from './components/directive.vue';
+import PropComponent from './components/props.vue';
 
 @Component({
   components: {
     FilterComponent,
     DirectiveComponent,
+    PropComponent,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private age = 18
+
+  private name = 'lorry'
+}
 </script>
 
 <style>
---
+++ src/components/props.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>this is parent name Prop: {{name}}</h1>
+        <input type="text" v-model="parentAge" >
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop, PropSync } from 'vue-property-decorator';
+
+@Component
+export default class PropComponent extends Vue {
+    @Prop({
+      // 配置类型
+      type: String,
+      // 配置是否必须
+      required: true,
+      // name必须包含'lorry', 可在parent的name data中进行修改, 不符合的将会告警
+      validator: (value: string) => value.includes('lorry'),
+    })
+    private name!:string;
+
+    // 可以实现父子组件的数据共享, 不能为同一标识符
+    @PropSync('age')
+    private parentAge!:string;
+}
+</script>
