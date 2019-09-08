4ae59e4067441bc5c8a444428c7e630597011ce2
JiangLirui <535904200@qq.com>
2019-09-08 18:16:42 +0800

    add filter

---
+++ src/components/filter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>这是演示filter</p>
+    <span>{{'hello' | addWorld}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+@Component({
+  filters: {
+    addWorld: (value: string) => `${value} world`,
+  },
+})
+export default class FilterComponent extends Vue {
+}
+</script>
